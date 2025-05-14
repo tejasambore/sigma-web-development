6e9d09b8bc86b73f80d7d64c84b6a9e401d44f68
Tejas <tejasambore2003@gmail.com>
2025-05-14 16:33:54 +0530

    JavaScript Loops

---
+++ Video57/index.js
@@ -0,0 +1,37 @@
+console.log("Tutorial on Loops..!!");
+
+let a = 1;
+// console.log(a);
+// console.log(a + 1);
+// console.log(a + 2);
+
+// for (let i = 0; i < 100; i++) {
+//     console.log(a + i);
+// }
+
+let obj = {
+    name: "Harry",
+    role: "Developer",
+    company: "Outlier AI",
+}
+
+// for (const key in obj) {
+//     console.log(key);
+//     // console.log(obj[key]);
+// }
+
+// for (const c of "Harry") {
+//     console.log(c);
+// }
+
+// let i = 0;
+// while (i < 6000) {
+//     console.log(i);
+//     i++;
+// }
+
+let i = 10;
+do {
+    console.log(i);
+    i++;
+} while(i < 6);
