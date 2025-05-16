599e6c1124b9cf9ec92eba1da65004802f461347
Tejas <tejasambore2003@gmail.com>
2025-05-16 21:06:55 +0530

    JavaScript Strings

---
+++ Video60/index.js
@@ -0,0 +1,29 @@
+console.log("Tutorial on JavaScript String..!");
+
+let a = "Harry";
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+console.log(a[5]);
+
+console.log(a.length);
+
+let name = "Harry";
+let friend = "Rohan";
+console.log("His name is " + name + " and his friend name is " + friend);
+console.log(`His name is ${name} and his friend name is ${friend}`);
+
+let b = "Shivansh";
+console.log(b.toUpperCase());
+console.log(b.toLowerCase());
+console.log(b.length);
+console.log(b.slice(1, 5));
+console.log(b.slice(1));
+
+
+console.log(b.replace("Sh", "77"));
+console.log(b.concat(a, "Aishwarya", "Rahul", "Priya"));
+
+console.log(b);
